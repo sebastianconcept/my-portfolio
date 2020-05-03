13516fdd5d41199ea09b8c24157e5254d661d488
Sebastian Sastre <sebastianconcept@gmail.com>
2020-05-03 20:25:12 -0300

    formating

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -10,39 +10,37 @@ const Layout: React.FunctionComponent<Props> = ({
   children,
   title = 'This is the default title',
 }) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>
-      <nav>
-        <Link href="/dashboard">
-          <a>Dashboard</a>
-        </Link>{' '}
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <header>
+        <nav>
+          <Link href="/dashboard">
+            <a>Dashboard</a>
+          </Link>{' '}
         |{' '}
-        <Link href="/operations">
-          <a>Operations</a>
-        </Link>{' '}
+          <Link href="/operations">
+            <a>Operations</a>
+          </Link>{' '}
         |{' '}
-        <Link href="/assets">
-          <a>Assets</a>
-        </Link>{' '}
+          <Link href="/assets">
+            <a>Assets</a>
+          </Link>{' '}
         |{' '}
-        <Link href="/categories">
-          <a>Categories</a>
-        </Link>{' '}
-        |{' '}
-        | <a href="/api/categories">Categories API</a>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
-)
+          <Link href="/categories">
+            <a>Categories</a>
+          </Link>
+        </nav>
+      </header>
+      {children}
+      <footer>
+        <hr />
+        <span>I'm here to stay (Footer)</span>
+      </footer>
+    </div>
+  )
 
 export default Layout
